3d654b1f180e4457ef25de87505f96df68c7c0c0
ponponl <hoianh2004@gmail.com>
2025-06-29 22:09:45 +0700

    ADD: view product feature

---
+++ FE/client/src/App.js
@@ -0,0 +1,25 @@
+import Home from './home/page';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import ViewProduct from './view-product/page';
+import './App.css';
+
+function App() {
+  const router = createBrowserRouter([
+    { 
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: '/view-product/:id',
+      element: <ViewProduct />
+    }
+  ]);
+
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
