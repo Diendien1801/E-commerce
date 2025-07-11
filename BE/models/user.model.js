f2626e8063ca361d6d56a4c3bf40255a42812b58
HoangYen2004 <lhy26042k4@gmail.com>
2025-07-12 00:20:00 +0700

    UPDATE: password attribute for userSchema

--- BE/models/user.model.js
+++ BE/models/user.model.js
@@ -9,7 +9,8 @@ const userSchema = new mongoose.Schema({
       /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i,
       "Invalid URL format for avatar"
     ]
-  }
+  },
+  password: {type: String, required: true}
 });
 
 module.exports = mongoose.model("User", userSchema);
