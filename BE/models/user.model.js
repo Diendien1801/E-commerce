b2ccd3546a4378d3225d23688867597980cb06f3
HoangYen2004 <lhy26042k4@gmail.com>
2025-07-11 22:35:49 +0700

    UPDATE: avatar attribute for userSchema

--- BE/models/user.model.js
+++ BE/models/user.model.js
@@ -3,6 +3,13 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
+  avatar: {
+    type: String,
+    match: [
+      /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i,
+      "Invalid URL format for avatar"
+    ]
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
