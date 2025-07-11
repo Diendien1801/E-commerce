338857c1399026b03dd20325fbc961281047897e
HoangYen2004 <lhy26042k4@gmail.com>
2025-07-11 23:18:08 +0700

    ADD: Change avatar feature

--- BE/controllers/user.controller.js
+++ BE/controllers/user.controller.js
@@ -8,3 +8,31 @@ exports.createUser = async (req, res) => {
     res.status(400).json({ error: err.message });
   }
 };
+
+//Change avatar
+exports.changeAvatar = async (req, res) => {
+  try {
+    const { avatar } = req.body;
+
+    if (!avatar || typeof avatar !== "string") {
+      return res.status(400).json({ error: "Avatar URL is required." });
+    }
+
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      return res.status(404).json({ error: "User not found." });
+    }
+
+    // Update avatar
+    user.avatar = avatar;
+
+    await user.save();
+
+    res.json({
+      message: "Avatar updated successfully.",
+      avatar: user.avatar
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
