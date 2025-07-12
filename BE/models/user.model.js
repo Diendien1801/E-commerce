8d75099b3d02b7b42b24588f19475e86ac626181
HoangYen2004 <lhy26042k4@gmail.com>
2025-07-12 20:45:10 +0700

    UPDATE: googleId and isVerified attributes for userSchema

--- BE/models/user.model.js
+++ BE/models/user.model.js
@@ -10,7 +10,10 @@ const userSchema = new mongoose.Schema({
       "Invalid URL format for avatar"
     ]
   },
-  password: {type: String, required: true}
+  password: {type: String, required: true},
+  
+  googleId: { type: String, unique: true, sparse: true},
+  isVerified: { type: Boolean, default: false},
 });
 
 module.exports = mongoose.model("User", userSchema);
