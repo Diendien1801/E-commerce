45dc175aeb1d96a9e6a00ec2e9aa8876931b091f
HoangYen2004 <lhy26042k4@gmail.com>
2025-07-12 20:50:17 +0700

    UPDATE: sign in with email feature

--- BE/app.js
+++ BE/app.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
 app.use(cors({ origin: "http://localhost:3000" }));
 const userRoutes = require("./routes/user.route"); // user
 const crawlRoute = require("./routes/crawl.route");// crawl data
 const productRoutes = require("./routes/product.route"); // product
+const authRoutes    = require("./routes/auth.route"); // auth
+require("./config/passport");
 app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/auth", authRoutes); 
 app.use(crawlRoute);
-
+app.use(session({
+  secret: "your-session-secret",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 module.exports = app;
 
-//Product
 
