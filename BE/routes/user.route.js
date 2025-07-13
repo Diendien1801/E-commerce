93764464e207c0d1f7536ebcccac8cc69544e0c3
HoangYen2004 <lhy26042k4@gmail.com>
2025-07-14 00:49:07 +0700

    ADD: route change avt and route change password

--- BE/routes/user.route.js
+++ BE/routes/user.route.js
@@ -4,4 +4,10 @@ const userController = require("../controllers/user.controller");
 
 router.post("/", userController.createUser);
 
+// Route: change avatar
+router.put("/changeavt", userController.changeAvatar);
+
+// Route: change password
+router.put("/changepass", userController.changePassword);
+
 module.exports = router;
