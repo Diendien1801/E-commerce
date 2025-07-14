14951e92aae42104f2b3d7b9016b47b27db75e6a
HoangYen2004 <lhy26042k4@gmail.com>
2025-07-14 20:03:11 +0700

    DOC: add notes for avatar and password

--- BE/models/user.model.js
+++ BE/models/user.model.js
@@ -3,13 +3,16 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
+  //new
   avatar: {
     type: String,
     match: [
       /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i,
       "Invalid URL format for avatar"
-    ]
+    ],
+    default: null
   },
+  //new
   password: {type: String, required: true}
 });
 
