7fd481f5d0be349e5cabff92a0f7c0e71d0f73ff
HoangYen2004 <lhy26042k4@gmail.com>
2025-07-15 16:48:04 +0700

    UPDATE: userSchema + ADD: password hashing and compare method

--- BE/models/user.model.js
+++ BE/models/user.model.js
@@ -1,8 +1,32 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const { v4: uuidv4 } = require('uuid');
 
 const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
+  //new
+  password: {type: String, required: true},
+  isVerified: { type: Boolean, default: false },
+  verificationToken: { type: String, default: () => uuidv4() },
+}, { timestamps: true });
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
+
+// Compare plain text password with hashed
+userSchema.methods.comparePassword = function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
 
 module.exports = mongoose.model("User", userSchema);
