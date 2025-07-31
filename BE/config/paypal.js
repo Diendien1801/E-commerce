081357b288f5e43d6026d8316754688ce5994fa9
HoangYen2004 <lhy26042k4@gmail.com>
2025-07-31 13:45:49 +0700

    ADD: PayPal SDK configuration

---
+++ BE/config/paypal.js
@@ -0,0 +1,18 @@
+const checkoutNodeJssdk = require('@paypal/checkout-server-sdk');
+
+const env = process.env.NODE_ENV === 'production'
+  ? new checkoutNodeJssdk.core.LiveEnvironment(
+      process.env.PAYPAL_CLIENT_ID,
+      process.env.PAYPAL_CLIENT_SECRET
+    )
+  : new checkoutNodeJssdk.core.SandboxEnvironment(
+      process.env.PAYPAL_CLIENT_ID,
+      process.env.PAYPAL_CLIENT_SECRET
+    );
+
+const client = new checkoutNodeJssdk.core.PayPalHttpClient(env);
+
+module.exports = {
+  paypalClient: client,
+  paypalSdk: checkoutNodeJssdk,
+};
