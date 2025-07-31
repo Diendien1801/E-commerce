88f8993f590e4b90ce526c8af5bd00dc04f55a6f
HoangYen2004 <lhy26042k4@gmail.com>
2025-07-31 13:38:01 +0700

    Update: paymentSchema

--- BE/models/payment.model.js
+++ BE/models/payment.model.js
@@ -7,40 +7,57 @@ const paymentSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
+
     userId: {
       type: String,
+      ref: "User",
       required: true,
     },
+
     orderId: {
       type: String,
+      ref: "Order",
       required: true,
     },
+
     method: {
       type: String,
       required: true,
-      enum: [
-        "Credit Card",
-        "VNPay",
-        "Momo",
-        "ZaloPay",
-        "Bank Transfer",
-        "Cash on Delivery",
-      ],
+      enum: ["MoMo", "PayPal", "Cash on Delivery"],
     },
+
     amount: {
       type: Number,
       required: true,
       min: 0,
     },
+
+    currency: {
+      type: String,
+      required: true,
+      enum: ["USD", "VND"],
+    },
+
     status: {
       type: String,
-      enum: ["pending", "completed", "failed", "canceled"],
+      enum: ["pending", "completed", "failed", "canceled", "refunded"],
       default: "pending",
     },
+
     paymentDate: {
       type: Date,
       default: null,
     },
+
+    transactionId: {
+      type: String,
+      default: null,
+    },
+
+    hasRefund: {
+      type: Boolean,
+      default: false,
+    }
   },
   {
     timestamps: true,
