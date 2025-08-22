ae2d1af2224d919c18c1a90f19361b9270217aae
ponponl <hoianh2004@gmail.com>
2025-08-22 14:19:05 +0700

    FIX: fix searching in order list

--- FE/client/src/App.js
+++ FE/client/src/App.js
@@ -32,6 +32,7 @@ import CrawlPage from './admin/crawl/page';
 
 import Payment from './payment/page';
 import PaymentResult from './paymentResult/payment-result'; 
+import { CartProvider } from './components/cart/CartContext';
 
 
 
@@ -94,7 +95,9 @@ function App() {
 
   return (
     <AuthProvider>
-      <RouterProvider router={router} />
+      <CartProvider>
+        <RouterProvider router={router} />
+      </CartProvider>
     </AuthProvider>
   );
 }
